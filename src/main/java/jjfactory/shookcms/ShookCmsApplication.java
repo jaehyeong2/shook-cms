d90f8c9ae160ea3e7442a0cfe76198a440d311b4
jae <wogud1514@naver.com>
2022-09-04 14:49:53 +0900

    시작

---
+++ src/main/java/jjfactory/shookcms/ShookCmsApplication.java
@@ -0,0 +1,13 @@
+package jjfactory.shookcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShookCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShookCmsApplication.class, args);
+	}
+
+}
